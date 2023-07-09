1485ea5655cefcdd8822329bb18b70422df4348c
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2023-07-09 14:13:37 +0545

    Routes added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/home", function (req, res, next) {
+  res.render("home", {});
+});
+
+router.get("/login", function (req, res, next) {
+  res.render("login");
+});
+
+router.get("/signUp", function (req, res, next) {
+  res.render("signUp");
+});
+
+router.get("/about", function (req, res, next) {
+  res.render("about");
+});
+
+router.get("/help", function (req, res, next) {
+  res.render("help");
+});
+
+router.get("/contactUs", function (req, res, next) {
+  res.render("contactUs");
+});
+
 module.exports = router;
