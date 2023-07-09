792c6f46b2b6bf863f8e3672c4e1ee3846c66874
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2023-07-09 23:12:33 +0545

    Dashboard added

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 router.get("/home", function (req, res, next) {
-  res.render("home", {});
+  res.render("index", {});
 });
 
 router.get("/login", function (req, res, next) {
@@ -30,4 +30,20 @@ router.get("/contactUs", function (req, res, next) {
   res.render("contactUs");
 });
 
+router.get("/dashboard", function (req, res, next) {
+  res.render("dashboard");
+});
+
+ router.post("/submit", async function (req, res, next) {
+   let forSignUp = new forSignUp ({
+     Username: req.body.username,
+     Email : req.body.email,
+     Password: req.body.password,
+      ConfirmPassword: req.body.confirmpassword,
+      DateOfBirth: req.body.dateofbirth,
+     Country: req.body.country
+   })
+ await forSignUp.insertMany(expenses);
+ res.redirect("/dashboard");
+  });
 module.exports = router;
